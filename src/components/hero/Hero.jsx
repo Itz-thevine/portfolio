eb674e1b195706fd8a878e1ff71aab27cf86afd2
Itz-thevine <divine2christ@yahoo.com>
2022-05-18 22:21:57 +0100

    new

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -42,23 +42,26 @@ const Hero = () => {
         <div className="hero container">
             <div className="hero__leftSection">
                 <div className='hero_leftControl'>
-                        <p className="hero__first">
-                            Hello World! I am
-                        </p>
-                        <div className='leftSectionCtrl'>
-                            <p className="hero__second">
-                            Divine
+                    
+                    <div className='leftSectionCtrl'>
+                        <div className='txtControl'>
+                            <p className="hero__first">
+                                Hello World! I am
                             </p>
-                            <div className='dot'></div>
+                            <p className="hero__second">
+                                Divine
+                            </p>
                         </div>
+                        <div className='dot'></div>
+                    </div>
 
-                        <p className='leftSection__subtext'>
-                        I am a web developer and Brand identity designer. My goal is to help individuals, firms and businesses scale up and also achieve all-round goals.
-                        </p>
+                    <p className='leftSection__subtext'>
+                    I am a web developer and Brand identity designer. My goal is to help individuals, firms and businesses scale up and also achieve all-round goals.
+                    </p>
 
-                        <div className='btnPostiton'>
-                            <Button name='Schedule A Call'/>
-                        </div>
+                    <div className='btnPostiton'>
+                        <Button name='Schedule A Call'/>
+                    </div>
                 </div>
 
                 <div className="hero__latestWork">
