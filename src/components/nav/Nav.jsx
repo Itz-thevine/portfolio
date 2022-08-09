2e15f8e478785a7f6dfd1ab19af653a8bb93fdd3
Itz-thevine <divine2christ@yahoo.com>
2022-08-09 16:48:40 +0100

    commit

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -13,6 +13,14 @@ import { Link as ExternalLink } from 'react-router-dom';
 
 
 const Nav = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    
+    window.onscroll = () =>{
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+        return () => (window.onscroll = null)
+    }
+
     gsap.config({
         nullTargetWarn: false,
         trialWarn: false,
@@ -60,67 +68,69 @@ const Nav = () => {
    
 
     return ( 
-        <div className={menu ? 'nav container addnavH' : 'nav container'} ref={navMiddleSection} id='nav'>
-            <div className='navControl'>
-                <div className="nav__name">
-                    <Link to='#hero' onClick={() => toggleMenu(menu)}>
-                        <img src={Logo} width={150} alt="" />
-                    </Link>
+        <div className={isScrolled ? 'scrolledClass' : ''}>
+            <div className={menu ? 'nav addnavH' : 'nav '} ref={navMiddleSection} id='nav'>
+                <div className='navControl'>
+                    <div className="nav__name">
+                        <Link to='#hero' onClick={() => toggleMenu(menu)}>
+                            <img src={Logo} width={150} alt="" />
+                        </Link>
+                    </div>
+
+                    <ul className={menu ? "nav__middle showMenu" : "hideMenu"}>
+                        <li>
+                            <Link to='#hero' className='nav_link' onClick={() => toggleMenu(menu)}>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='#about' className='nav_link' onClick={() => toggleMenu(menu)}>About me</Link>
+                        </li>
+                        <li>
+                            <Link to='#skill' className='nav_link' onClick={() => toggleMenu(menu)}>Skills</Link>
+                        </li>
+                        <li>
+                            <Link to='#works' className='nav_link' onClick={() => toggleMenu(menu)}>Works</Link>
+                        </li>
+                        <li>
+                            <Link to='#contact' className='nav_link' onClick={() => toggleMenu(menu)}>Contact</Link>
+                        </li>
+                    </ul>
+
+                    <div className='nav__icons'>
+                        <a href='https://twitter.com/ik_thevine' target='_blank'>
+                            <div className='nav__icon'>
+                                <BsIcons.BsTwitter />
+                            </div>
+                        </a>
+                        <a href="https://www.instagram.com/ik_thevine/" target='_blank'>
+                            <div className='nav__icon'>
+                                <BsIcons.BsInstagram />
+                            </div>
+                        </a>
+                        <a href="https://www.behance.net/ik_thevine" target='_blank'>
+                            <div className='nav__icon'>
+                                <BsIcons.BsBehance />   
+                            </div>
+                        </a>
+                        <a href="https://www.linkedin.com/in/ikechukwu-divine-38a885222/" target='_blank'>
+                            <div className='nav__icon'>
+                                <BsIcons.BsLinkedin />
+                            </div>
+                        </a>
+                        <a href="https://github.com/Itz-thevine" target='_blank'>
+                            <div className='nav__icon'>
+                                <AiIcons.AiFillGithub/>
+                            </div>
+                        </a>
+
+                    </div>
+                    <div className={menu ? 'naav__mobileIcon open_nav' : 'naav__mobileIcon'} onClick={() => toggleMenu(menu)}>
+                        <CgIcons.CgMenuRight/> 
+                    </div>
+                    
                 </div>
 
-                <ul className={menu ? "nav__middle showMenu" : "hideMenu"}>
-                    <li>
-                        <Link to='#hero' className='nav_link' onClick={() => toggleMenu(menu)}>Home</Link>
-                    </li>
-                    <li>
-                        <Link to='#about' className='nav_link' onClick={() => toggleMenu(menu)}>About me</Link>
-                    </li>
-                    <li>
-                        <Link to='#skill' className='nav_link' onClick={() => toggleMenu(menu)}>Skills</Link>
-                    </li>
-                    <li>
-                        <Link to='#works' className='nav_link' onClick={() => toggleMenu(menu)}>Works</Link>
-                    </li>
-                    <li>
-                        <Link to='#contact' className='nav_link' onClick={() => toggleMenu(menu)}>Contact</Link>
-                    </li>
-                </ul>
-
-                <div className='nav__icons'>
-                    <a href='https://twitter.com/ik_thevine' target='_blank'>
-                        <div className='nav__icon'>
-                            <BsIcons.BsTwitter />
-                        </div>
-                    </a>
-                    <a href="https://www.instagram.com/ik_thevine/" target='_blank'>
-                        <div className='nav__icon'>
-                            <BsIcons.BsInstagram />
-                        </div>
-                    </a>
-                    <a href="https://www.behance.net/ik_thevine" target='_blank'>
-                        <div className='nav__icon'>
-                            <BsIcons.BsBehance />   
-                        </div>
-                    </a>
-                    <a href="https://www.linkedin.com/in/ikechukwu-divine-38a885222/" target='_blank'>
-                        <div className='nav__icon'>
-                            <BsIcons.BsLinkedin />
-                        </div>
-                    </a>
-                    <a href="https://github.com/Itz-thevine" target='_blank'>
-                        <div className='nav__icon'>
-                            <AiIcons.AiFillGithub/>
-                        </div>
-                    </a>
-
-                </div>
-                <div className={menu ? 'naav__mobileIcon open_nav' : 'naav__mobileIcon'} onClick={() => toggleMenu(menu)}>
-                    <CgIcons.CgMenuRight/> 
-                </div>
-                
             </div>
-
-        </div>
+        </div> 
      );
 }
  
